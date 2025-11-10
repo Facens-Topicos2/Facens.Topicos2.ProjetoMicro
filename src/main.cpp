4178b973be730211e059b46493782e82a7ab100d
pedroghiotti <pedro.ghiotti.m@gmail.com>
2025-11-10 19:49:29 -0300

    feat: código inicial adicionado fazendo uso de funções do FreeRTOS

--- src/main.cpp
+++ src/main.cpp
@@ -1,18 +1,25 @@
 #include <Arduino.h>
 
-// put function declarations here:
-int myFunction(int, int);
-
 void setup() {
-  // put your setup code here, to run once:
-  int result = myFunction(2, 3);
+  gpio_config_t io_conf = {
+      .pin_bit_mask = (1ULL << GPIO_NUM_2), // Configure GPIO2
+      .mode = GPIO_MODE_OUTPUT,             // Set as output mode
+      .pull_up_en = GPIO_PULLUP_DISABLE,    // Disable pull-up
+      .pull_down_en = GPIO_PULLDOWN_DISABLE,// Disable pull-down
+      .intr_type = GPIO_INTR_DISABLE        // Disable interrupt
+  };
+
+  gpio_config(&io_conf); // Apply the configuration
+
+  while (1) {
+    // Set GPIO2 high
+    gpio_set_level(GPIO_NUM_2, 1);
+    vTaskDelay(500 / portTICK_PERIOD_MS); // Delay for 0.5 second
+
+    // Set GPIO2 low
+    gpio_set_level(GPIO_NUM_2, 0);
+    vTaskDelay(500 / portTICK_PERIOD_MS); // Delay for 0.5 second
+  }
 }
 
-void loop() {
-  // put your main code here, to run repeatedly:
-}
-
-// put function definitions here:
-int myFunction(int x, int y) {
-  return x + y;
-}
+void loop() {}
